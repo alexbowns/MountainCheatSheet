882833d01e688342c08910f6bf1f8a7aeab4c277
Alex Bowns <alex.bowns16@gmail.com>
2019-06-26 09:07:20 -0700

    Creating LogSelectActivity.java

---
+++ app/src/main/java/com/alexbowns/mountainstats/Presentation/LogSelectActivity.java
@@ -0,0 +1,17 @@
+package com.alexbowns.mountainstats.Presentation;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.alexbowns.mountainstats.R;
+
+public class LogSelectActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_log_select);
+    }
+}
